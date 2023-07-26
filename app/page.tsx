24eeac3a50b1a40f374b410502634c9ac810b29a
Theo Gaillard <theogllrd@gmail.com>
2023-07-26 23:47:04 +0200

    first input/select hided

--- app/page.tsx
+++ app/page.tsx
@@ -285,7 +285,7 @@ export default function Home() {
     <div className='h-screen p-2'>
       <div className="w-full text-2xl text-center">ETF Scanner</div>
 
-      <div className="mt-2">
+      {/*<div className="mt-2">
         <select
           id="etfs"
           onChange={e => onChangeSelect(e)}
@@ -306,7 +306,7 @@ export default function Home() {
           onChange={e => setAmount(e.target.value)}
           className="mt-2 w-full bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
-      </div>
+          </div>*/}
 
 
 
