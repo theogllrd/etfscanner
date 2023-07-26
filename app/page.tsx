c65264ff1a89aedc0115ad9df8853fdcd990b325
Theo Gaillard <theogllrd@gmail.com>
2023-07-26 23:44:39 +0200

    feature totalAmount by action

--- app/page.tsx
+++ app/page.tsx
@@ -97,72 +97,31 @@ export default function Home() {
 
 
 
-  /*const etfList = [
-      {
-        id: 1, name: 'SP500', compo: [
-      {name: 'Apple', pourcentage: 0.00378 },
-      {name: 'Microsoft', pourcentage: 0.00157 },
-      ]
-    },
-      {id: 2, name: 'NASDAQ' },
-      {id: 3, name: 'MSCI World' },
-      ];*/
 
   const etfList = [
     {
       id: 1,
       name: 'S&P 500',
       composition: [{
+        compoId: 1,
         compoName: 'Apple',
-        compoValue: 0.0661
+        compoValue: 0.60
       }, {
+        compoId: 2,
         compoName: 'Microsoft',
-        compoValue: 0.0558
-      }, {
-        compoName: 'Amazon',
-        compoValue: 0.0251
-      }, {
-        compoName: 'Nvidia',
-        compoValue: 0.0173
-      }, {
-        compoName: 'Tesla',
-        compoValue: 0.0166
-      }, {
-        compoName: 'Berkshire Hathaway',
-        compoValue: 0.0165
-      }, {
-        compoName: 'Alphabet',
-        compoValue: 0.0161
-      }, {
-        compoName: 'Exxon',
-        compoValue: 0.0136
-      }, {
-        compoName: 'Unitedhealth',
-        compoValue: 0.0133
-      }, {
-        compoName: 'Jpmorgan',
-        compoValue: 0.0126
-      }, {
-        compoName: 'Autres',
-        compoValue: 0.747
+        compoValue: 0.40
       }]
     },
     {
       id: 2,
-      name: 'NASDAQ'
-    },
-    {
-      id: 3,
-      name: 'MSCI World',
+      name: 'NASDAQ',
       composition: [{
-        compoName: 'microsoft',
-        compoValue: 1.234
+        compoId: 1,
+        compoName: 'Apple',
+        compoValue: 0.55
       }, {
-        compoName: 'Tesla',
-        compoValue: 4.58
-      }, {
-        compoName: 'Boug',
-        compoValue: 2.51
+        compoName: 'Amazon',
+        compoValue: 0.45
       }]
     },
   ];
@@ -170,6 +129,9 @@ export default function Home() {
 
   const [etf, setEtf] = useState(etfList[0]); // define the state variable and initialized it with the first value of the table
   const [amount, setAmount] = useState(''); // define the state variable and initialized it with the first value of the table
+
+
+
 
 
   const onChangeSelect = (e: any) => {
@@ -184,6 +146,7 @@ export default function Home() {
 
   /* Début de la decouverte */
   const [isDisabled, setIsDisabled] = useState(false)
+  const [totalAmount, setTotalAmount] = useState(0)
   useEffect(() => {
     if (inputList.length > 0) {
       inputList[inputList.length - 1].input === ""
@@ -191,43 +154,126 @@ export default function Home() {
         : setIsDisabled(false)
     }
   })
+
+  // init the state with a first input inside
   const [inputList, setInputList] = useState([
     {
       input: "",
+      etf: "1",
       input_rank: null
     }
   ])
+
   const handleListAdd = () => {
     setInputList([
       ...inputList,
       {
         input: "",
+        etf: "1",
         input_rank: null
       }
     ])
   }
   const handleInputChange = (event: any, index: any) => {
-    const { value } = event.target
+    /*const { value } = event.target
     const newInputList = [...inputList]
     newInputList[index].input = value
     newInputList[index].input_rank = index + 1
     setInputList(newInputList)
+    let amount = 0;
+    inputList.map((input) => {
+      amount += parseInt(input.input);
+    })
+    setTotalAmount(amount)*/
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    const newInputList = [...inputList]
+    target.name === 'etfSelection' ? newInputList[index].etf = value : newInputList[index].input = value;
+    //newInputList[index].input = value
+    //newInputList[index].etf = value
+    newInputList[index].input_rank = index + 1
+    setInputList(newInputList)
+    //console.log(inputList);
+    resultatsAvecEtf();
   }
+
   const handleRemoveItem = (index: any) => {
     const newList = [...inputList]
     newList.splice(index, 1)
     setInputList(newList)
+    // ici refaire le calcul du totalAmount sinon il s'update pas au delete
   }
 
-  console.log(inputList)
+  //console.log(inputList)
   /* fin de la décougette*/
 
 
 
 
 
+  const [resultatParAction, setResultatParAction] = useState([]);
+
+  const resultatsAvecEtf = () => {
+    let resu = [];
+    let temp = 0;
+    for (let i = 1; i <= etfList.length; i++) {
+      inputList.map((e) => {
+        parseInt(e.etf) == i ? temp += parseInt(e.input) : null;
+      })
+      resu.push([i, temp])
+      temp = 0
+    }
+
+    resultatsParAction(resu)
+  }
 
 
+  const resultatsParAction = (resu: any) => {
+    //console.log('resu avec etf')
+    //console.log(resu)
+    let tempData: any = []
+    let finalTableToDisplay: any = []
+    let shouldPush = true
+    //let resuEtf = []
+    // parcourir le tableau resu
+    resu.map((element: any) => {
+      let idEtf = element[0];
+      let value = element[1];
+      const fullDataEtf = etfList.filter((e) => e.id == idEtf)[0]; // je récupère la data de l'etf que je parcours
+      fullDataEtf.composition.map((r) => { // je parcours la composition de l'etf en question
+        // je dois comparer si l'id de mon r est déjà présent dans mon tableau finalTableToDisplay
+        /*finalTableToDisplay.map((f: any) => {
+          //console.log(f[0] + ' == ' + r.compoName)
+          f[0] == r.compoName ? f[1] += r.compoValue : null
+
+        })*/
+
+        finalTableToDisplay.map((f: any) => {
+          if (f[0] == r.compoName) {
+            f[1] += r.compoValue * value
+            shouldPush = false
+          }
+          //f[0] == r.compoName ? f[1] = r.compoValue * value && shouldPush = false : null
+
+        })
+
+
+        shouldPush ? finalTableToDisplay.push([r.compoName, r.compoValue * value]) : null
+        shouldPush = true
+
+      })
+
+      //resuEtf[index].push
+    })
+    // pour chaque element dans resu, je prend son id et je le compare a un id dans etfList
+
+    // je dois push chaque ligne dans un tableau
+    //console.log('final table to display')
+    //console.log(finalTableToDisplay)
+    setResultatParAction(finalTableToDisplay)
+    //console.log(resultatParAction)
+  }
 
 
 
@@ -275,9 +321,12 @@ export default function Home() {
         ? inputList.map((input, index) => (
           <div key={index}
             className="w-full flex mt-2">
+
             <select
+              name="etfSelection"
               id="etfs"
-              onChange={e => onChangeSelect(e)}
+              //onChange={e => onChangeSelect(e)}
+              onChange={(event: any) => handleInputChange(event, index)}
               className="w-full bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               {etfList.map((etf) =>
@@ -287,17 +336,20 @@ export default function Home() {
                 </option>)}
             </select>
             <input
+              name="amountSelection"
+              type="number"
+              id="amount"
               placeholder='Amount'
-              className="w-full bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="ml-2 w-full bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              //onChange={(event: any) => handleInputChange(event, index)}
               onChange={(event: any) => handleInputChange(event, index)}
               value={input.input}
             />
-            <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+            <button className="ml-2 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
               onClick={() => handleRemoveItem(index)}>
-
               Delete
-
             </button>
+
           </div>
         ))
         : null}
@@ -322,6 +374,7 @@ export default function Home() {
 
 
 
+      {resultatParAction}
 
 
 
@@ -329,14 +382,13 @@ export default function Home() {
 
 
 
-
-      {
+      {/*
         etf.composition ?
           <ResultsTable etfs={etf} totalAmount={amount} /> :
           <p className="pt-2">No data</p>
-      }
+    */}
 
-      <ResultsChart etfs={etf} />
+      {/*<ResultsChart etfs={etf} />*/}
 
 
     </div >
